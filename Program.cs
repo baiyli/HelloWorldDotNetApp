bcd95ce8715ef0341925852607ec42fb3c57e2e6
baiyli <105086653+baiyli@users.noreply.github.com>
2022-10-18 17:17:12 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => ".NET runtime version: "+Environment.Version.ToString());
 
 app.Run();
